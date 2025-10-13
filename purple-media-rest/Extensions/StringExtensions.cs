a8b2cd49960c73c95b28795368c24163c90dfffa
generalmaks <schrinan@gmail.com>
2025-10-13 16:36:45 +0300

    Added documentation to StringExtensions.cs

---
+++ purple-media-rest/Extensions/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace purple_media_rest.Extensions;
+
+public static class StringExtensions
+{
+    /// <summary>
+    /// Returns all starting indices of a specified substring within the given text, performing a case-insensitive search.
+    /// </summary>
+    /// <param name="text">The source string to search in.</param>
+    /// <param name="snippet">The substring to look for.</param>
+    /// <returns>An array of integers representing the starting positions of all occurrences of the substring in text</returns>
+    public static int[] AllIndices(this string text, string snippet)
+    {
+        var indices = new List<int>();
+        int index = 0;
+        while ((index = text.IndexOf(snippet, index, StringComparison.OrdinalIgnoreCase)) != -1)
+        {
+            indices.Add(index);
+            index += snippet.Length;
+        }
+        return indices.ToArray();
+    }
+    
+    /// <summary>
+    /// Returns all starting indices of multiple substrings within the given text, performing a case-insensitive search.
+    /// </summary>
+    /// <param name="text">The source string to search in.</param>
+    /// <param name="snippets">An array of substrings to look for.</param>
+    /// <returns>An array of integers representing the starting positions of all occurrences of each substring in text</returns>
+    public static List<int> AllIndices(this string text, string[] snippets)
+    {
+        var indices = new List<int>();
+        foreach (var snippet in snippets)
+        {
+            indices.AddRange(text.AllIndices(snippet));
+        }
+        return indices;
+    }
+}
