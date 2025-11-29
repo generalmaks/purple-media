52ddc5bfe21039072f67a102cdf08891a2e3196a
generalmaks <schrinan@gmail.com>
2025-11-29 18:55:49 +0200

    Updated user page

--- purple-media-ng/src/app/pages/user-page/user-page.component.ts
+++ purple-media-ng/src/app/pages/user-page/user-page.component.ts
@@ -1,25 +1,46 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {User, UserService} from '../../services/user.service';
 import {DatePipe} from '@angular/common';
+import {ActivatedRoute} from "@angular/router";
+import {Tweet, TweetService} from "../../services/tweet.service";
+import {TweetComponent} from "../../components/tweet/tweet.component";
 
 @Component({
   selector: 'app-user-page',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, TweetComponent],
   templateUrl: './user-page.component.html',
   styleUrl: './user-page.component.css'
 })
 export class UserPageComponent implements OnInit {
-  userId: number | null = 0
+  userId: number
   user: User
-  userTweets: any[] = []
+  userTweets: Tweet[] = []
   profilePictureUrl?: string
 
   private userService = inject(UserService)
+  private activatedRoute = inject(ActivatedRoute)
+  private tweetService = inject(TweetService)
 
   ngOnInit(): void {
-    this.userService.get(this.userId!).subscribe(user => {
-      this.user = user!;
+    this.activatedRoute.paramMap.subscribe(params => {
+      this.userId = Number(params.get("id"))
+
+      this.loadUser()
+      this.loadUserTweets()
+    })
+  }
+
+  private loadUser() {
+    this.userService.get(this.userId).subscribe(user => {
+      this.user = user!
+      this.profilePictureUrl = user?.profilePictureUrl
+    })
+  }
+
+  private loadUserTweets() {
+    this.tweetService.getUserTweets(this.userId).subscribe(tweets => {
+      this.userTweets = tweets
     })
   }
 }
