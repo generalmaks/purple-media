2d8b88d727b8bcea665012cbced9f0a61a00564b
generalmaks <schrinan@gmail.com>
2025-12-09 16:33:18 +0200

    Added link to chat on sidebar

--- purple-media-ng/src/app/components/sidebar/sidebar.component.ts
+++ purple-media-ng/src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
 
+export interface SidebarButton {
+  link: string,
+  label: string,
+  openOnNewPage: boolean
+}
+
 @Component({
   selector: 'app-sidebar',
   standalone: true,
@@ -9,12 +15,13 @@ import { NgFor } from '@angular/common';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-  public buttons = [
-    { link: "http://localhost:4200/user/general_maks", text: "😎Boss account" },
-    { link: "http://youtube.com", text: "📺Youtube" }
+  public buttons: SidebarButton[] = [
+    { link: "http://localhost:4200/user/general_maks", label: "😎Boss account", openOnNewPage: true },
+    { link: "http://youtube.com", label: "📺Youtube", openOnNewPage: true },
+    { link: "/chat", label: "💬Chat", openOnNewPage: false}
   ]
 
-  Clicked(button: { link: string; text: string }) {
-    window.open(button.link, "_blank");
+  Clicked(button: SidebarButton) {
+    window.open(button.link, button.openOnNewPage ? "_blank" : "_self");
   }
 }
