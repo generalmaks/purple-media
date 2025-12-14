2ccfa17b130729c3c4600f576cfb1887bab22ea3
generalmaks <schrinan@gmail.com>
2025-12-14 19:28:28 +0200

    Updated chat

--- purple-media-ng/src/app/pages/chat-page/chat-page.component.ts
+++ purple-media-ng/src/app/pages/chat-page/chat-page.component.ts
@@ -44,7 +44,7 @@ export class ChatPageComponent implements OnInit {
     this.chatHub.messages$.subscribe(msg => {
       if (!msg) return
 
-      if (this.selectedChat && this.selectedChat.otherUserId === Number(msg.sender)){
+      if (this.selectedChat && this.selectedChat.otherUserId === Number(msg.sender)) {
         this.selectedChatMessages.push({
           senderId: Number(msg.sender),
           receiverId: this.currentUser.id,
@@ -57,24 +57,9 @@ export class ChatPageComponent implements OnInit {
     })
   }
 
-  loadChats() {
-    this.chatMessages.getChatInfo().subscribe({
-      next: (chats: ChatInfo[]) => this.chats = chats,
-      error: err => console.error('Could not log chats: ' + JSON.stringify(err))
-    })
-  }
-
   selectChat(chat: ChatInfo) {
     this.selectedChat = chat
     this.loadMessages(chat.otherUserId)
-  }
-
-  loadMessages(otherUserId: number) {
-    this.chatMessages
-      .getMessagesFromChatAsync(this.currentUser.id, otherUserId, 1, this.pageSize)
-      .subscribe((msg: any) => {
-        this.selectedChatMessages = msg.reverse()
-      })
   }
 
   send() {
@@ -86,10 +71,35 @@ export class ChatPageComponent implements OnInit {
     this.chatHub.sendMessage(otherUserId, text);
 
     this.selectedChatMessages.push({
-      content: this.newMessage, id: 0, isRead: false, messageSent: Date.now().toString(), receiverId: this.selectedChat.otherUserId, senderId: this.currentUser.id
+      content: this.newMessage,
+      id: 0,
+      isRead: false,
+      messageSent: Date.now().toString(),
+      receiverId: this.selectedChat.otherUserId,
+      senderId: this.currentUser.id
 
     });
 
     this.newMessage = '';
+  }
+
+  closeChat() {
+    this.selectedChat = null;
+    this.selectedChatMessages = [];
+  }
+
+  private loadChats() {
+    this.chatMessages.getChatInfo().subscribe({
+      next: (chats: ChatInfo[]) => this.chats = chats,
+      error: err => console.error('Could not log chats: ' + JSON.stringify(err))
+    })
+  }
+
+  private loadMessages(otherUserId: number) {
+    this.chatMessages
+      .getMessagesFromChatAsync(this.currentUser.id, otherUserId, 1, this.pageSize)
+      .subscribe((msg: any) => {
+        this.selectedChatMessages = msg.reverse()
+      })
   }
 }
